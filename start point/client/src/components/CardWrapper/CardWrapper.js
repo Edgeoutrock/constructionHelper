e6dc826485ba0474da43141b3fb746751394a827
RAvil <russian82@mail.com>
2020-04-22 16:19:27 -0400

    added .gitignore

--- start point/client/src/components/CardWrapper/CardWrapper.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./CardWrapper.css";
-
-const CardWrapper = props => <div className="wrapper">{props.children}</div>;
-
-export default CardWrapper;
-
--- start point/client/src/components/Wrapper/Wrapper.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Wrapper = props => <div id="wrapper">{props.children}</div>;
-
-export default Wrapper;
