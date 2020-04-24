4d184541cb791070e150904cbd0e3247f1089172
Edgeoutrock <phana@mymail.vcu.edu>
2020-04-23 20:28:48 -0400

    Add files via upload
    code combination

--- client/src/components/ProjectPost/ProjectPost.js
+++ client/src/components/ProjectPost/ProjectPost.js
@@ -1,5 +1,13 @@
 import React, { useRef, useState } from "react";
 import { useStorageState } from 'react-storage-hooks';
+//import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import Collapsible from 'react-collapsible';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import {DebounceInput} from 'react-debounce-input';
+import ReactWordcloud from 'react-wordcloud';
+import { Resizable } from 're-resizable';
 // import OpenWeatherMap from 'react-open-weather-map';
 import image1 from '../assets/pic1.jpg';
 import image2 from '../assets/pic2.jpg';
@@ -24,21 +32,132 @@ import { useOpenWeatherMapAPI } from "@jakubzet/use-openweathermap-api";
 
 
 
-
+const resizeStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  border: 'solid 1px #ddd',
+  background: '#f0f0f0',
+};
 
 
 
 
 function ProjectPost() {
+
+  //var debounce = require('lodash.debounce');
   const inputRef = useRef();
   const descriptionRef = useRef();
   const imageURLRef = useRef();
   const serviceRef = useRef();
-  
+  var dateNow = new Date();
+  var dateString = dateNow.toISOString().substring(0, 10);
+  const useStyles = makeStyles((theme) => ({
+    container: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    textField: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+      width: 200,
+    },
+  }));
+  const classes = useStyles();
 
-  const [title, setTitle] = useState('')
+  const [completeList, finishProject] = useState([]);
 
+  const collectFinished = (id, name) => {
+
+    finishProject([...completeList, {id, name}]);
+  }
+
+// const reviewBubble = () => {
+//   var takenBubble = [];
+// for (var i = 0; i < force.length < i++){
+//   var wordTaken = force[i].toLowerCase().split(" ");
+//   for(var j = 0; j < wordTaken[i].length; j++){
+
+//     takenBubble.push(wordTaken[j]);
+//   }
   
+// }
+
+// }
+
+
+const [force, setForce, errorHandler] = useStorageState(
+  localStorage,
+  'word-bubble',
+  []
+);
+
+const removeLBubbleLocal = () => {
+  localStorage.removeItem('word-bubble');
+
+  toast("words removed after renavigation!");
+}
+
+const [appear, setAppear] = useState(false);
+const [shown, setToggle] = useState(false);
+//const [force, setForce] = useState([]);
+
+var chosenWords = force;
+var sureTaken = chosenWords.join(" ").toLowerCase().split(" ");
+
+var newArray = [];
+
+for (var i = 0 ; i < sureTaken.length; i ++){
+  var word = sureTaken[i];
+  newArray.push(word);
+}
+
+
+
+
+//console.log(newArray);
+
+
+function getWordCount(array) {
+  let map = {};
+  for (let i = 0; i < array.length; i++) {
+    let item = array[i];
+    map[item] = (map[item] + 1) || 1;
+  }
+  return map;
+}
+
+const change = getWordCount(newArray);
+
+/*                       Array for Bubble                                               */
+var soughtBubble = [];
+/*                       Array for Bubble                                               */
+for(var i = 0; i < Object.keys(change).length; i++){
+  var keys = Object.keys(change);
+  var values = Object.values(change);
+  
+  const entries = new Map([
+  ['text', keys[i]],
+  ['value', values[i]]
+]);
+
+const obj = Object.fromEntries(entries);
+soughtBubble.push(obj);
+}
+  
+  var filteredBubble = soughtBubble.filter(x => x.text.length > 3);
+
+
+
+
+
+
+
+
+
+  const [title, setTitle] = useState('');
+const [query, setParameter] = useState('');
+const searchQuery = "https://" + `${title}` + ".craigslist.org/search/sss?query="+`${query.split(" ").join("+")}`+"&s=0&sort=rel";
    // info: sample is the response object from the OpenWeatherMap's API
   //const [projects, setProjects] = useState([]);
   const [projects, setProjects, writeError] = useStorageState(
@@ -46,6 +165,8 @@ function ProjectPost() {
     'display-projects',
     []
   );
+
+  
 
   const show = () => {
 
@@ -66,7 +187,7 @@ function ProjectPost() {
   const removeLocalStorage = () => {
     localStorage.removeItem('display-projects');
 
-    toast("removed projects from display!");
+    toast("projects removed after renavigation");
   }
 
   
@@ -115,7 +236,7 @@ function ProjectPost() {
 
 
   return (<>
-    <div className = "zindex1">
+  <div className = "zindex1">
       <h1>Create a Project!</h1>
       <form className="form-group mt-5" onSubmit={addProject}>
         <input
@@ -155,31 +276,108 @@ function ProjectPost() {
         
       </form>
       
+      
     </div>
     
-    <div className = "zindex3">
+    {appear && (<>
 
+<div className = "zindex3">
+
+<main>
+  <h1>Weather</h1>
+
+
+
+  <section>
+    <h4>Data:</h4>
+    {state.data && state.data.name && state.data.main ? (
+      <>
+        <p>Weather in {state.data.name}:</p>
+        <ul>
+          <li>Currently: {state.data.main.temp  * 9/5 + 32} degrees</li>
+          <li>Max: {state.data.main.temp_max * 9/5 + 32} degrees</li>
+          <li>Min: {state.data.main.temp_min * 9/5 + 32} degrees</li>
+        </ul>
+      </>
+    ) : (
+      <p>Nope</p>
+    )}
+  </section>
+  
+  
+</main>
+
+</div>
+</>)}
+
+
+    <div className = "zindex6">
+
+
+{shown ? (<Resizable
+        defaultSize={{
+          width: 300,
+          height: 230,
+        }}
+        style={resizeStyle}>
+        <div style={{ width: '100%', height: '100%' }}>
+          <ReactWordcloud words={filteredBubble} />
+        </div>
+        {/* <button type="button" class="btn btn-danger" onClick = {removeLBubbleLocal}>Clear Bubble</button> */}
+      </Resizable>) : (<div> Review for Word Bubble </div>) }
+    
     <main>
-      <h1>Weather</h1>
+      <h1>Project Completion</h1>
  
     
  
       <section>
-        <h4>Data:</h4>
-        {state.data && state.data.name && state.data.main ? (
+        <h4>Finished</h4>
+        
+    
+        {completeList ? (
           <>
-            <p>Weather in {state.data.name}:</p>
-            <ul>
-              <li>Currently: {state.data.main.temp  * 9/5 + 32} degrees</li>
-              <li>Max: {state.data.main.temp_max * 9/5 + 32} degrees</li>
-              <li>Min: {state.data.main.temp_min * 9/5 + 32} degrees</li>
-            </ul>
+          {shown && (<button type="button" class="btn btn-danger" onClick = {removeLBubbleLocal}>Clear Bubble</button>)}
+            {completeList.map((listItem) => (<>
+              
+              <Collapsible trigger= {listItem.name && "Completed: " + listItem.name}>
+              
+              What do you think of {listItem.name}?
+              
+              <p>
+                
+        {/* <input
+          className="form-control"
+          onChange={event => {setForce([...force, event.target.value]); console.log(force)} }
+          
+          type = "text"
+          placeholder="leave a review"
+        /> */}
+
+          <DebounceInput
+          className="form-control"
+          minLength={2}
+          debounceTimeout={500}
+          onChange={event => {setForce([...force, event.target.value]); console.log(force); setToggle(true)}} 
+          placeholder="leave a review"/>
+              
+              {/* <button onClick = {console.log(force)}> change here</button>
+              
+              */}
+              </p>
+              </Collapsible>
+              
+
+         </>   ))}
           </>
         ) : (
           <p>Nope</p>
         )}
+    
+        
+      
       </section>
- 
+      
       
     </main>
 
@@ -192,8 +390,16 @@ function ProjectPost() {
       <span >  <Link style={{ color: '#000000' }} to="/">Back to home</Link> </span>
       <button type="button" class="btn btn-danger" onClick = {removeLocalStorage}>Clear All Projects</button>
       <button className="btn btn-success mt-3 mb-5" onClick={fetchWeather} >
-          Fetch Weather
+      <div onClick = {() => {setAppear(true)}}>  Fetch Weather</div> 
         </button>
+
+        <input
+          className="form-control"
+          onChange={event => setParameter(event.target.value) }
+          
+          type = "text"
+          placeholder="city field required to search Craigslist for item"
+        />
       <ToastContainer />
       {/* <ReactWeather forecast="5days" apikey="a824229569c4a405459ff5720da5b0df" type="geo" lat="48.1351"
   lon="11.5820"/> */}
@@ -207,25 +413,60 @@ function ProjectPost() {
       {writeError && (
         <pre>Cannot write to localStorage: {writeError.message}</pre>
       )}
-      <ul className = "list-group">
+      <ul   className = "list-group">
+      
+      
+      
+  
+      
+    
       {projects.map((item, index) => (
-          <li className="list-group-item" key={item.id}>
+         
+          
+
             
-            <div key={item.name} className="card mx-auto col-4">
+          <li style = {{width: "790px", padding: "10px 15px"}}className="list-group-item" key={item.id}>
+            <Collapsible trigger= {item.servicesNeeded ? "Service needed " + item.servicesNeeded : "no services desired"}>
+            <div key={item.name} style = {{height: "375px", margin: "10px"}}className="card mx-auto col-4">
             <img className="card-img-top" src={item.imageURL} alt={item.name} />
             <div className="card-body">
               <h4 className="card-title"> <span className={item.priority ? "font-weight-bold" : ""}> {item.name}</span></h4>
             <ul className="card-text">
               <li>
               <p >
-                {item.description}
+                {item.description} 
+      
+      <p>
+       {query && (
+        <a style={{ color: '#000000' }} href= { searchQuery }  target="_blank">
+         search from Craigslist
+        </a>)
+        } 
+
+        <form className={classes.container} noValidate>
+      <TextField
+        id="date"
+        label="Complete by Date"
+        type="date"
+        defaultValue = {dateString}
+        className={classes.textField}
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
+      <span className={item.priority ? "font-weight-bold card-text" : ""}> Service: {item.servicesNeeded ? item.servicesNeeded : "none" }</span> 
+      <button style = {{height: "30px"}}className="btn btn-success mt-3 mb-5" onClick={(e) => {e.preventDefault(); collectFinished(item.id, item.name);}}> 
+Complete
+      </button>
+    </form>
+      </p>
               </p>
               </li>
-              <li>
-              <span className={item.priority ? "font-weight-bold card-text" : ""}> Service: {item.servicesNeeded ? item.servicesNeeded : "none" }</span> 
-              </li>
+            
+              
+            
               </ul>
-         
+              
          {/*
           <li>
           <button
@@ -246,15 +487,22 @@ function ProjectPost() {
               X Remove
             </button>
           </li> */}
+
+
            </div>
            </div>
+           </Collapsible>
           </li>
+          
+          
         ))}
+       
+        
       </ul>
     </div>
 <BackgroundSlideshow images={[ image1, image2, image3, image4, image5, image6, image7, image8 ]} />
-</>
-  );
+
+ </> );
 }
 
 export default ProjectPost;
